71f5fe24458ba6240c7c4052629a829b0385a473
Udit Manav <uditmanav17@gmail.com>
2022-01-02 19:30:19 +0530

    Time: 757 ms (8.18%), Space: 15.7 MB (80.56%) - LeetHub

--- combinations/combinations.py
+++ combinations/combinations.py
@@ -1,5 +1,15 @@
-from itertools import combinations
-
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        return list(combinations(range(1, n+1), k))
+        elements = list(range(1, n + 1))
+        result = []
+        self.combineHelper(elements, k, [], result)
+        return result
+
+    def combineHelper(self, elements, k, curr_res, result):
+        if len(curr_res) == k:
+            result.append(curr_res)
+
+        for idx in range(len(elements)):
+            self.combineHelper(
+                elements[idx + 1 :], k, curr_res + [elements[idx]], result
+            )
