31c1a0cbbac7c56ec37f70a5641dd0c014e29bb9
Udit Manav <uditmanav17@gmail.com>
2022-01-06 21:24:37 +0530

    Time: 60 ms (6.10%), Space: 14.2 MB (76.72%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/spiral-matrix-ii/discuss/963128/Python-rotate-when-need-explained
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        mat = [[0] * n for _ in range(n)]
+        dx, dy = 1, 0
+        x, y = 0, 0
+        
+        for idx in range(n * n):
+            # assign to cell
+            mat[y][x] = idx + 1
+            
+            # check if out of bounds or already assigned
+            if not 0 <= x + dx < n or not 0 <= y + dy < n or mat[y+dy][x+dx] != 0:
+                # change directions
+                dx, dy = -dy, dx
+                
+            # update x and y
+            x, y = x + dx, y + dy
+            
+        return mat
+                
