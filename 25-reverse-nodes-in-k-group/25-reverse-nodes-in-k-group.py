b0fdb6bdc371ffd8e26e23619ec1b65b235b4fa2
Udit Manav <uditmanav17@gmail.com>
2022-01-08 23:52:29 +0530

    Time: 52 ms (58.24%), Space: 15.4 MB (13.96%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -5,21 +5,34 @@
 #         self.next = next
 
 class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        nodes = []
-        while head:
-            nodes.append(head)
-            head = head.next
-            
-        n = len(nodes)
-        for idx in range(0, n - (n % k), k):
-            nodes[idx:idx+k] = reversed(nodes[idx:idx+k])
-            
-        for idx in range(1, len(nodes)):
-            prev_node = nodes[idx-1]
-            curr_node = nodes[idx]
-            prev_node.next = curr_node
+    def reverse_nodes(self, head, count):
+        prev, curr = None, head
         
-        nodes[-1].next = None
+        while curr and count > 0:
+            next_ = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_
+            count -= 1
+        
+        return prev, head
             
-        return nodes[0]
+    
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        # check if we have sufficient nodes to reverse
+        count, node = 0, head
+        while node and count < k:
+            node = node.next
+            count += 1
+        # if insufficient nodes return current list
+        if count < k: return head
+        
+        reversed_list_head, reversed_list_tail = self.reverse_nodes(head, k)
+        # print(reversed_list_head.val, reversed_list_tail.val)
+        reversed_list_tail.next = self.reverseKGroup(node, k)
+        
+        return reversed_list_head
+        
+        
+        
+        
