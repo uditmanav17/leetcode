d55e9215c3e478c83cf612cdace9b625f51df14c
Udit Manav <uditmanav17@gmail.com>
2022-01-08 17:18:54 +0530

    Time: 142 ms (21.00%), Space: 19.9 MB (6.94%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,12 +1,23 @@
-
 from collections import Counter
+from pprint import pprint as pp
 
 class Solution:
+    # bucket sort
+    # since k is in the range [1, the number of unique elements in the array]
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = Counter(nums)
+        buckets = [[] for _ in range(len(nums)+1)]
+        counts = Counter(nums).items()
         
-        top_k = counts.most_common(k)
+        # add elements in buckets based on their frequencies
+        for ele, freq in counts:
+            buckets[freq].append(ele)
+        # pp(buckets)
         
-        # print(top_k)
+        # flatten all lists of elements
+        flattened_list = [ele for sublist in buckets for ele in sublist]
+        # pp(flattened_list)
         
-        return [key for key, val in top_k]
+        return flattened_list[::-1][:k]
+        
+        
+        
