211a0b257829fc1ed31d0069a7a2986f89d94ec2
Udit Manav <uditmanav17@gmail.com>
2022-01-09 17:47:12 +0530

    Time: 456 ms (87.55%), Space: 18.8 MB (87.98%) - LeetHub

---
+++ 134-gas-station/134-gas-station.py
@@ -0,0 +1,22 @@
+# start city will be one in which we enter with min gas
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total_curr_gas = 0
+        start_idx = -1
+        min_gas_level = float("inf")
+        
+        for idx, (curr_gas, curr_cost) in enumerate(zip(gas, cost)):
+            if total_curr_gas < min_gas_level:
+                min_gas_level = total_curr_gas
+                start_idx = idx
+            total_curr_gas += (curr_gas - curr_cost)
+                
+        return start_idx
+            
+            
+            
