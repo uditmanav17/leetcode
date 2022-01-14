29f3add5388ad8356f8ca2a4e329f3cb811e21da
Udit Manav <uditmanav17@gmail.com>
2022-01-14 22:47:00 +0530

    Time: 532 ms (69.55%), Space: 18.4 MB (81.31%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/maximum-sum-circular-subarray/discuss/178422/One-Pass
+
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        # kadane with max/min subarr sum
+        
+        total = nums[0]
+        curr_min = curr_max = all_min = all_max = nums[0]
+        
+        for idx in range(1, len(nums)):
+            curr_max = max(nums[idx], curr_max + nums[idx])
+            all_max = max(all_max, curr_max)
+            
+            curr_min = min(nums[idx], curr_min + nums[idx])
+            all_min = min(all_min, curr_min)
+            
+            total += nums[idx]
+            
+        # print(all_max, all_min, total)
+        
+        # corner case - when all nums are -ve OR sum(nums) == all_min
+        return max(all_max, total - all_min) if total - all_min > 0 else all_max
+    
