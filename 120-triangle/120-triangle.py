c02321e2a7d097e03102b91c30d21570c2d11a9d
Udit Manav <uditmanav17@gmail.com>
2022-01-22 15:39:07 +0530

    Time: 64 ms (86.80%), Space: 15.2 MB (54.44%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        ans = [0] * (len(triangle) + 1)
+        
+        for row in reversed(triangle):
+            for idx, num in enumerate(row):
+                ans[idx] = num + min(ans[idx], ans[idx + 1])
+        
+        # print(ans)
+        
+        return ans[0]
