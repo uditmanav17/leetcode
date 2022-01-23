6c3c6ea73add394f51098b6e2a4fcefa331fb095
Udit Manav <uditmanav17@gmail.com>
2022-01-23 14:09:39 +0530

    Time: 16 ms (100.00%), Space: 14.2 MB (80.16%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,22 +1,12 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        # all digits required for number creation
-        q = deque([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        # all sequential numbers
+        nums = deque([12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789])
+        
         ans = []
-
-        while q:
-            # print(q)
-            curr_num = q.popleft()
+        while nums:
+            curr_num = nums.popleft()
             if low <= curr_num <= high:
                 ans.append(curr_num)
-
-            last_digit = curr_num % 10
-            if last_digit < 9:
-                # create next_num and add to q
-                next_num = curr_num * 10 + (last_digit + 1)
-                if next_num > high:
-                    continue
-                q.append(next_num)
-
         return ans
 
