1ea94b6cdb820cd8116e1b96ca3eee509a37a134
Udit Manav <uditmanav17@gmail.com>
2022-01-30 17:49:00 +0530

    Time: 504 ms (60.16%), Space: 20.7 MB (8.56%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -0,0 +1,24 @@
+
+# The goal is to keep track of:
+
+# * Maximum So far and add it to the cur_cell and maintain maximum result
+# * Here, max_so_far contains : A[i] + i
+
+# **Original Given Formula : A[i] + A[j] + i - j**
+
+# * Break in two parts : A[i] + i and A[j] -j
+# * Keep MAX_VALUE of first part among the elements seen so far
+# * Add the current element to max_so_far and check the result is changing or not
+# * Also, keep updating the max_so_far at each step
+
+class Solution:
+
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        max_so_far,result = values[0], 0
+        
+        for i in range(1,len(values)):
+            result = max(result, max_so_far + values[i] - i)
+            max_so_far = max(max_so_far, values[i] + i)
+			
+        return result
+
