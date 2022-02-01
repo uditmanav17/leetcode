1fa1ae3b4875cc7803e6dc205809a206b0562ca6
Udit Manav <uditmanav17@gmail.com>
2022-02-01 11:26:29 +0530

    Time: 2008 ms (7.83%), Space: 25 MB (80.69%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,12 @@
-
-# find max value, after min arr val
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        prev_min = prices[0]
+        profit = float("-inf")
+        buy_price = float("inf")
         
-        for ele in prices:
-            prev_min = min(prev_min, ele)
-            profit = max(profit, ele - prev_min)
+        for sell_price in prices:
+            buy_price = min(buy_price, sell_price)
+            profit = max(profit, sell_price - buy_price)
             
         return profit
-
+                
+            
