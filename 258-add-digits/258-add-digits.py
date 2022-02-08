c7d12ab5ae6cb55e0bbc9b824e8fb3e518650d53
Udit Manav <uditmanav17@gmail.com>
2022-02-08 11:13:31 +0530

    Time: 58 ms (19.01%), Space: 13.8 MB (99.39%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,7 +1,13 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        return self.approach2(num)
+        
+    def approach1(self, num: int):
         while num > 9:
-            num, r = divmod(num, 10)
-            num += r
+            num = sum(int(c) for c in str(num))
         return num
+        
+    # check notes/solution
+    def approach2(self, num: int):
+        return 0 if num == 0 else (num - 1) % 9 + 1
         
