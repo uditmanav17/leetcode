46aa7acc85ea4b46abcb08a327dc0cf0c8338cc4
Udit Manav <uditmanav17@gmail.com>
2022-02-10 11:59:34 +0530

    Time: 482 ms (12.08%), Space: 16.6 MB (93.61%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,19 +1,25 @@
-# soln approach 3
-
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         
-        counts = {0:1}
         total = ans = 0
+        counts = {0:1}
         
-        for idx in range(len(nums)):
-            # calculate jump in cumsum
-            total += nums[idx]
+        for num in nums:
+            # get cumsum
+            total += num
             
-            # check difference bw current sum and target 
+            # get diff bw target and curr_cumsum
             diff = total - k
+            
+            # check if diff is already seen and add it to ans
             ans += counts.get(diff, 0)
             
+            # add curr_cumsum to counts
             counts[total] = counts.get(total, 0) + 1
-            
+        
         return ans
+            
+            
+        
+        
+        
