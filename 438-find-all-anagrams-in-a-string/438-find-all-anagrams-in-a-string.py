7425a7bc2e12e50b87af86ef48db07e98ab77e3b
Udit Manav <uditmanav17@gmail.com>
2022-02-11 10:31:24 +0530

    Time: 385 ms (27.47%), Space: 15.3 MB (7.82%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,34 +1,30 @@
+from collections import Counter
+from string import ascii_lowercase
+
 class Solution:
-    
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        p_counts = Counter(p)
-        s_counts = Counter(s[:len(p)])
-        ans = []
+        p_counts = Counter({char:0 for char in ascii_lowercase})
+        p_counts.update(p)
         
+        s_counts = Counter({char:0 for char in ascii_lowercase})
+        s_counts.update(s[:len(p)])
+        
+        ans = []
         if p_counts == s_counts:
-            ans.append(0)
-            
+                ans.append(0)
+        
         # take one char at a time from 's'
         # and compare it with char counts of 'p'
         # if counts match, add first idx of sub string
-        for add_char_idx in range(len(p), len(s)):
-            remove_char_idx = add_char_idx - len(p)
-            add_char = s[add_char_idx]
-            rem_char = s[remove_char_idx]
-            # print(add_char_idx, remove_char_idx)
+        for idx in range(len(p), len(s)):
+            remove_char = s[idx - len(p)]
+            add_char = s[idx]
+            # print(add_char, remove_char)
             
-            s_counts[add_char] = s_counts.get(add_char, 0) + 1
-            s_counts[rem_char] -= 1
-            if s_counts[rem_char] == 0:
-                s_counts.pop(rem_char)
+            s_counts[add_char] = s_counts[add_char] + 1
+            s_counts[remove_char] = s_counts[remove_char] - 1
             
-            # print(p_counts, s_counts)
             if p_counts == s_counts:
-                ans.append(remove_char_idx+1)
-            
+                ans.append(idx - len(p) + 1)
+                
         return ans
-            
-            
-            
-            
-            
