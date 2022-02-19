da39b550c7f8984ef6ef67c69071d7054d063319
Udit Manav <uditmanav17@gmail.com>
2022-02-19 11:49:12 +0530

    Time: 36 ms (66.33%), Space: 13.9 MB (76.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,20 +1,14 @@
-
 class Solution:
-    def approach1(self, m: int, n: int) -> int:
-        return factorial(m + n - 2) // (factorial(m - 1) * factorial(n - 1))
-    
-    def approach2(self, m: int, n: int) -> int:
-        dp = [[0]*n for _ in range(m)]
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
         
         for r, c in product(range(m), range(n)):
-            if r==0 or c==0:
+            if r == 0 or c == 0:
                 dp[r][c] = 1
-                continue
-            dp[r][c] = dp[r-1][c] + dp[r][c-1]
-            
+            else:
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+                
+        # print(dp)
         return dp[-1][-1]
-    
-    
-    def uniquePaths(self, m: int, n: int) -> int:
-        return self.approach2(m, n)
+                
         
