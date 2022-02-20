a097c717726c6be92c46580d5019c82602803ab2
Udit Manav <uditmanav17@gmail.com>
2022-02-20 11:34:31 +0530

    Time: 146 ms (43.38%), Space: 14.3 MB (96.36%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.py
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.py
@@ -3,18 +3,21 @@ class Solution:
         # custom sort - first on start point, next on (end-start)
         intervals.sort(key=lambda x: (x[0], -(x[1] - x[0])))
         # print(intervals)
-        ans = [intervals[0]]
+        # ans = [intervals[0]]
+        prev_start, prev_end = intervals[0]
+        ans = 1
         
         for idx in range(1, len(intervals)):
-            prev_start, prev_end = ans[-1]
+            # prev_start, prev_end = ans[-1]
             curr_start, curr_end = intervals[idx]
             
             if curr_start >= prev_start and curr_end <= prev_end:
                 continue
             
-            ans.append(intervals[idx])
+            prev_start, prev_end = intervals[idx]
+            ans += 1
             
-        return len(ans)
+        return ans
         
         
             
