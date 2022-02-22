b50729113da3b40eb7582cbedb5b4ced9589e4f1
Udit Manav <uditmanav17@gmail.com>
2022-02-22 14:46:59 +0530

    Time: 54 ms (51.91%), Space: 14.3 MB (59.81%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,23 +1,19 @@
-# https://leetcode.com/problems/word-break/discuss/1455100/Python-2-solutions-(Optimized-with-Trie)-Clean-and-Concise
-# try opimization with trie
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
         wordDict = set(wordDict)
-        
+        n = len(s)
+
         @lru_cache(None)
-        def dp_recursive(start):
-            if start == n:
+        def word_break_helper(start_idx):
+            if start_idx == n:
                 return True
-            
-            for idx in range(start, n):
-                curr_word = s[start:idx+1]
-                # print(curr_word)
-                if curr_word in wordDict and dp_recursive(idx + 1):
+
+            for idx in range(n):
+                curr_word = s[start_idx : idx + 1]
+                if curr_word in wordDict and word_break_helper(idx + 1):
                     return True
-                
+
             return False
 
+        return word_break_helper(0)
 
-        return dp_recursive(0)
