28b718feabceefeb38e0cee53a46b60af46d5385
Udit Manav <uditmanav17@gmail.com>
2022-02-22 23:43:17 +0530

    Time: 729 ms (45.69%), Space: 15.3 MB (65.83%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,17 +1,18 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        idx = 0
+        maxReach = nums[idx]
         n = len(nums)
-        maxPos = i = 0
-
-        # iterate over nums arr, till we can proceed
-        while i <= maxPos:
-            # at each index, check maximum possible reach
-            # and assign it to maxPos
-            maxPos = max(maxPos, i + nums[i])
-            # if we have cross last index, return True
-            if maxPos >= n - 1:
+        
+        while idx <= maxReach:
+            maxReach = max(maxReach, idx + nums[idx])
+            
+            # reached last index
+            if maxReach >= n - 1:
                 return True
-            i += 1
-
+            
+            idx += 1
+            
         return False
-
+                
+        
