1165fae88ca4f5490d36f70c88b5f3dfb7dc78fd
Udit Manav <uditmanav17@gmail.com>
2022-02-25 18:19:58 +0530

    Time: 432 ms (79.62%), Space: 23.1 MB (32.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,13 +1,23 @@
+from pprint import pprint as pp
+from itertools import product
+
+
 class Solution:
-    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        l1, l2 = len(s1), len(s2)
+    def longestCommonSubsequence(self, t1: str, t2: str) -> int:
+        l1 = len(t1) + 1
+        l2 = len(t2) + 1
+        dp = [[0] * l1 for _ in range(l2)]
 
-        lcs = [[0] * (l1+1) for _ in range(l2+1)]
+        for i, j in product(range(1, l2), range(1, l1)):
+            # print(i, j)
+            ch1, ch2 = t2[i - 1], t1[j - 1]
 
-        for idx1 in range(1, l2+1):
-            for idx2 in range(1, l1+1):
-                if s2[idx1-1] == s1[idx2-1]:
-                    lcs[idx1][idx2] = lcs[idx1-1][idx2-1] + 1 
-                else:
-                    lcs[idx1][idx2] = max(lcs[idx1-1][idx2], lcs[idx1][idx2-1])
-        return lcs[-1][-1]
+            if ch1 == ch2:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        # pp(dp)
+
+        return dp[-1][-1]
+
