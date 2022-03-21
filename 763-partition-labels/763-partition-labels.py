5bc28bd1c5f782d0e5eb3a22e43b2861526d746c
Udit Manav <uditmanav17@gmail.com>
2022-03-21 16:26:30 +0530

    Time: 62 ms (44.49%), Space: 13.9 MB (78.97%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,43 +1,15 @@
-# Compute interval (start, end) for each letter [a-z], 
-# where start is first occurrence of letter, and end is last occurrence of letter. 
-# Then we merge any overlapping intervals, 
-# and the resulting intervals can form the answer.
-
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return intervals
-        
-        intervals.sort(key=lambda x: x[0])
-        ans = [intervals[0]]
-        
-        for idx in range(1, len(intervals)):
-            currStart, currEnd = intervals[idx]
-            oldStart, oldEnd = ans[-1]
-            if currStart <= oldEnd:
-                ans[-1] = [oldStart, max(currEnd, oldEnd)]
-            else:
-                ans.append([currStart, currEnd])
-        
-        return ans
-    
     def partitionLabels(self, s: str) -> List[int]:
-        # get (start, end) for each char
-        maps = {}
+        last_seen = {char:idx for idx, char in enumerate(s)}
+        
+        ans = [-1]
+        
+        covered_idx = 0
         for idx, char in enumerate(s):
-            if char in maps:
-                new_interval = maps.get(char)
-                new_interval[1] = idx
-                maps[char] = new_interval
-            else:
-                maps[char] = [idx, idx]
-                
-        # print(maps)
-        intervals = [v for k, v in maps.items()]
-        merged_intervals = self.merge(intervals)
-        # print(merged_intervals)
+            covered_idx = max(covered_idx, last_seen[char])
+            
+            if covered_idx == idx:
+                ans.append(covered_idx)
         
-        return [(j - i + 1) for i, j in merged_intervals]
-        
-        
+        return [ans[idx] - ans[idx - 1] for idx in range(1, len(ans))]
         
