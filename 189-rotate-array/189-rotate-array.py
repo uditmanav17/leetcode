c739ed59c748c6093d0c993fba913a6e12cada90
Udit Manav <uditmanav17@gmail.com>
2022-04-29 19:05:27 +0530

    Time: 318 ms (47.24%), Space: 25.4 MB (78.64%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,20 +1,22 @@
 class Solution:
-    def rev(self, arr, start_idx, end_idx):
-        while start_idx < end_idx:
-            arr[start_idx], arr[end_idx] = arr[end_idx], arr[start_idx]
-            start_idx += 1
-            end_idx -= 1
-            
-    
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverseInRange(arr, start_idx, end_idx):
+            # start and end are inclusive
+            while start_idx <= end_idx:
+                arr[start_idx], arr[end_idx] = arr[end_idx], arr[start_idx]
+                start_idx += 1
+                end_idx -= 1
+                
+        
         n = len(nums)
-        k = k % n
-        self.rev(nums, 0, n - k - 1)
-        self.rev(nums, n - k, n - 1)
-        self.rev(nums, 0, n - 1)
-        
-        
+        k %= n
+        cut_point = n - k - 1
+        # print(n, k, cut_point)
+        reverseInRange(nums, 0, cut_point)
+        reverseInRange(nums, cut_point + 1, n - 1)
+        reverseInRange(nums, 0, n - 1)
+            
         
