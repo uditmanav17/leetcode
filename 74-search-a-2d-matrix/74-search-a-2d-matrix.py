d1fde5a77e565a79c6349180019cc7405916799e
Udit Manav <uditmanav17@gmail.com>
2022-05-01 17:50:41 +0530

    Time: 93 ms (7.41%), Space: 14.5 MB (45.66%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,23 +1,21 @@
-# https://leetcode.com/problems/search-a-2d-matrix/discuss/26248/6-12-lines-O(log(m)-%2B-log(n))-myself%2Blibrary
-
-# treat the matrix like a single big list of length m*n and use a simple binary search
-
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         rows, cols = len(matrix), len(matrix[0])
-        
         l, r = 0, rows * cols - 1
         
         while l <= r:
             mid = l + (r - l) // 2
             
-            num = matrix[mid//cols][mid%cols]
+            curr_r, curr_c = divmod(mid, cols)
+            ele = matrix[curr_r][curr_c]
+            # print(mid, curr_r, curr_c)
+            # print(l, mid, r, ele)
             
-            if num == target:
+            if ele == target:
                 return True
-            elif num < target:
+            elif ele < target:
                 l = mid + 1
-            elif num > target:
+            else:
                 r = mid - 1
                 
         return False
