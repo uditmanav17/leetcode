e15757fb37039269ac4107da086b2b957963b3b1
Udit Manav <uditmanav17@gmail.com>
2022-05-01 16:46:40 +0530

    Time: 42 ms (87.22%), Space: 14.2 MB (58.33%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,29 +1,45 @@
 class Solution:
-    def searchHelper(self, arr, l, r, target):
+    def binSearch(self, arr, l, r, target):
+        while l <= r:
+            m = l + (r - l) // 2
+            if arr[m] == target:
+                return m
+            elif arr[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
+                
+        
+    
+    def search(self, arr: List[int], target: int) -> int:
+        # ans = self.binSearch([1,2,3,4,5,6,7,8,9], 0, 4, 5)
+        # print(ans)
+
+        l, r = 0, len(arr) - 1
         
         while l <= r:
-            mid = l + ((r - l) >> 1)
-            
-            potential_match = arr[mid]
-            left_num = arr[l]
-            right_num = arr[r]
-            
-            if potential_match == target:
+            mid = l + (r-l) // 2
+            if arr[mid] == target:
                 return mid
-            elif left_num <= potential_match:
-                if left_num <= target < potential_match:
-                    r = mid - 1
+            
+            # left part of mid is sorted
+            if arr[l] <= arr[mid]:
+                if arr[l] <= target < arr[mid]:
+                    return self.binSearch(arr, l, mid - 1, target)
                 else:
                     l = mid + 1
-            else:
-                if potential_match < target <= right_num:
-                    l = mid + 1
+            
+            # right part of mid is sorted
+            elif arr[mid] <= arr[r]:
+                if arr[mid] < target <= arr[r]:
+                    return self.binSearch(arr, mid + 1, r, target)
                 else:
                     r = mid - 1
                     
         return -1
-            
-        
+                
+                
     
-    def search(self, nums: List[int], target: int) -> int:
-        return self.searchHelper(nums, 0, len(nums) - 1, target)
+                
+                
