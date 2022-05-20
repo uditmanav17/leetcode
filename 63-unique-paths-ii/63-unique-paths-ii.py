1e9189e69ddedb8e3be6e4df5e0cf666984a2aea
Udit Manav <uditmanav17@gmail.com>
2022-05-20 11:05:00 +0530

    Time: 57 ms (50.99%), Space: 14.1 MB (19.70%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -1,25 +1,25 @@
-
-from itertools import product
-
 class Solution:
     def uniquePathsWithObstacles(self, mat: List[List[int]]) -> int:
         if mat[0][0] == 1:
             return 0
         
-        rows = len(mat)
-        cols = len(mat[0])
-
-        dp = [[0] * cols for _ in range(rows)]
-        dp[0][0] = 1
-
+        rows, cols = len(mat), len(mat[0])
+        ans = [[0] * cols for _ in range(rows)]
+        ans[0][0] = 1
+        # print(ans)
+        
         for r, c in product(range(rows), range(cols)):
-            if mat[r][c] == 1:
+            if mat[r][c] == 1 or (r == 0 and c == 0):
                 continue
-                
-            if r > 0: 
-                dp[r][c] += dp[r-1][c]
+            if r == 0 and c > 0:
+                ans[r][c] = ans[r][c-1]
+            elif r > 1 and c == 0:
+                ans[r][c] = ans[r-1][c]
+            else:
+                ans[r][c] = ans[r-1][c] + ans[r][c-1]
+        
+        # print(ans)
+        
+        return ans[-1][-1]
             
-            if c > 0: 
-                dp[r][c] += dp[r][c-1]
-
-        return dp[-1][-1]
+        
