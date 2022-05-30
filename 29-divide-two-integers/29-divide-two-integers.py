ca2d47bbd0ca319dd5829aac3728c9a956a0e069
Udit Manav <uditmanav17@gmail.com>
2022-05-30 23:11:30 +0530

    Time: 41 ms (66.22%), Space: 13.9 MB (76.13%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,7 +1,17 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        # print(round(10 / 3), round(7 / -3))
-        print(dividend/divisor)
+        if dividend == 0:
+            return 0
         
-        return min(trunc(dividend/divisor), 2147483647)
+		# we work with positive numbers only and worry about the sign in the end
+        positiveSign = (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+
+        quotient = 0
+        for i in reversed(range(dividend.bit_length() - divisor.bit_length() + 1)):
+            if divisor <= (dividend >> i):
+                dividend -= divisor << i
+                quotient |= 1 << i
         
+		# min handles corner case -2 ** 31 / -1 = 2 ** 31
+        return min(2 ** 31 - 1, quotient) if positiveSign else -quotient
