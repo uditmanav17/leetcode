324543c74fe5ea19a81078bdd995313a70822af5
Udit Manav <uditmanav17@gmail.com>
2022-06-05 14:10:31 +0530

    Time: 44 ms (42.12%), Space: 13.9 MB (60.78%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,20 +3,14 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l, r = 0, n
+        l, r = 1, n
         
-        while l <= r:
+        while l < r:
             mid = l + (r - l) // 2
             
             curr_bad = isBadVersion(mid)
-            prev_bad = isBadVersion(mid - 1)
-            next_bad = isBadVersion(mid + 1)
-            
-            # print(l, mid, r)
-            if curr_bad and not prev_bad:
-                return mid
-            elif curr_bad and prev_bad:
-                r = mid - 1
+            if curr_bad:
+                r = mid 
             else:
                 l = mid + 1
-                
+        return l
