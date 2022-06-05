18dfdf5dda052b0285d7335536540389c3469c98
Udit Manav <uditmanav17@gmail.com>
2022-06-05 14:58:20 +0530

    Time: 55 ms (32.40%), Space: 13.9 MB (72.64%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,3 +1,21 @@
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a, 2) + int(b, 2))[2:]
+        a, b = a[::-1], b[::-1]
+        idx1 = idx2 = carry = 0
+        ans = []
+        
+        while idx1 < len(a) or idx2 < len(b) or carry:
+            n1 = int(a[idx1]) if idx1 < len(a) else 0
+            n2 = int(b[idx2]) if idx2 < len(b) else 0
+            
+            carry, curr_bit = divmod(n1 + n2 + carry, 2)
+            ans.append(curr_bit)
+            
+            idx1 += 1
+            idx2 += 1
+                        
+        return "".join(map(str, ans[::-1]))
+            
+            
+        
