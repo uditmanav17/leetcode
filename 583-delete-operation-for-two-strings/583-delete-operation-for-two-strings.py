b0445d3de95202472d5fd2122cd93668df4c427a
Udit Manav <uditmanav17@gmail.com>
2022-06-14 11:35:09 +0530

    Time: 669 ms (8.07%), Space: 16 MB (63.87%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,26 +1,20 @@
 class Solution:
-    def lcs(self, t1: str, t2: str) -> int:
-        l1 = len(t1) + 1
-        l2 = len(t2) + 1
-        dp = [[0] * l1 for _ in range(l2)]
-
-        for i, j in product(range(1, l2), range(1, l1)):
-            # print(i, j)
-            ch1, ch2 = t2[i - 1], t1[j - 1]
-
-            if ch1 == ch2:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-        # pp(dp)
-
-        return dp[-1][-1]
-    
-    
-    def minDistance(self, word1: str, word2: str) -> int:
-        if word1 == word2:
-            return 0
+    def lcs(self, w1, w2):
+        dp = [[0 for _ in range(len(w2) + 1)] for _ in range(len(w1) + 1)]
         
-        return len(word1 + word2) - 2 * self.lcs(word1, word2)
+        for idx1, idx2 in product(range(1, len(w1) + 1), range(1, len(w2) + 1)):
+            c1 = w1[idx1 - 1]
+            c2 = w2[idx2 - 1]
+            if c1 == c2:
+                dp[idx1][idx2] = dp[idx1-1][idx2-1] + 1
+            else:
+                dp[idx1][idx2] = max(dp[idx1-1][idx2], dp[idx1][idx2-1])
+                    
+        # print(dp)
+        return dp[-1][-1]
+        
+    def minDistance(self, word1: str, word2: str) -> int:
+        lcs_len = self.lcs(word1, word2)
+        return len(word1) + len(word2) - 2 * lcs_len
+        
         
