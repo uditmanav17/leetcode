4e9cfe32509938ef4faa1df21c5ac57968d6f7cf
Udit Manav <uditmanav17@gmail.com>
2022-07-17 12:27:39 +0530

    Time: 1085 ms (30.00%), Space: 75.8 MB (33.75%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/k-inverse-pairs-array/discuss/1282496/Python-Short-O(nk)-solution-explained
+# https://leetcode.com/problems/k-inverse-pairs-array/discuss/1282998/Python%3A-Explained-Bottom-Up-DP
+
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        dp = [[1] * (k+1) for _ in range(n+1)]
+        sp = [[1] * (k+1) for _ in range(n+1)]
+        N = 10**9 + 7
+
+        for i in range(1, n+1):
+            for j in range(1, k+1):
+                dp[i][j] = sp[i-1][j] if j < i else (sp[i-1][j] - sp[i-1][j-i]) % N
+                sp[i][j] = (sp[i][j-1] + dp[i][j]) % N
+        
+        return dp[-1][-1]
+        
