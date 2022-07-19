ec7b26d87a79ee9c1316ad9f38df5c3a3b9c87b7
Udit Manav <uditmanav17@gmail.com>
2022-07-19 14:35:55 +0530

    Time: 40 ms (71.95%), Space: 13.8 MB (97.62%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,15 +1,14 @@
 class Solution:
     def __init__(self):
-        self.pascal = [[1], [1,1]]
+        self.triangle = [[1], [1, 1]]
         
-        while len(self.pascal) < 30:
-            prev_row = self.pascal[-1]
-            curr_row = [0] * (len(prev_row) + 1)
-            curr_row[0] = curr_row[-1] = 1
-            for idx in range(1, len(curr_row) - 1):
-                curr_row[idx] = prev_row[idx-1] + prev_row[idx]
-            self.pascal.append(curr_row[:])
+        while len(self.triangle) < 30:
+            prev_row = self.triangle[-1]
+            next_row = [a + b
+                        for a, b in zip(prev_row + [0], [0] + prev_row)]
+            self.triangle.append(next_row)
             
     def generate(self, numRows: int) -> List[List[int]]:
-        return self.pascal[:numRows]
+        return self.triangle[:numRows]
+        
         
